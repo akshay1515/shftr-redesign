e32aad07f2dafae5f4eb4f44e1fa70ebe08e253b
Davisdavy <davisdavy96@gmail.com>
2022-01-19 17:29:40 +0300

    Bottom sheet UI

--- lib/features/shifter/presentation/pages/candidateactivity/local_candidate_page.dart
+++ lib/features/shifter/presentation/pages/candidateactivity/local_candidate_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:shifter/features/shifter/presentation/pages/locationactivity/location_activity.dart';
 import 'package:shifter/features/shifter/presentation/widgets/customchips.dart';
 import 'package:shifter/utils/colorconstant.dart';
@@ -12,6 +13,12 @@ class LocalCandidatePage extends StatefulWidget {
 }
 
 class _LocalCandidatePageState extends State<LocalCandidatePage> {
+   String _site = "";
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -178,7 +185,8 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
                 ),
               ),
               Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 20.0),
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 12.0, vertical: 20.0),
                 child: Container(
                   width: MediaQuery.of(context).size.width,
                   child: Column(
@@ -200,7 +208,9 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
                             fontFamily: 'Poppins',
                             fontSize: FontConstant.Mini_Tagline_text),
                       ),
-                      SizedBox(height: 20.0,),
+                      SizedBox(
+                        height: 20.0,
+                      ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
@@ -215,24 +225,27 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                            Text(
-                              "User",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w700,
-                                  fontFamily: 'Poppins',
-                                  fontSize: FontConstant.Mini_Tagline_text),
-                            ),
-                            SizedBox(width: 5.0,),
-                            Text(
-                              "has no experience",
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w400,
-                                  fontFamily: 'Poppins',
-                                  fontSize: FontConstant.Mini_Tagline_text),
-                            ),
-                          ],)
+                              Text(
+                                "User",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w700,
+                                    fontFamily: 'Poppins',
+                                    fontSize: FontConstant.Mini_Tagline_text),
+                              ),
+                              SizedBox(
+                                width: 5.0,
+                              ),
+                              Text(
+                                "has no experience",
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: 'Poppins',
+                                    fontSize: FontConstant.Mini_Tagline_text),
+                              ),
+                            ],
+                          )
                         ],
                       )
                     ],
@@ -246,8 +259,9 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
                 width: MediaQuery.of(context).size.width,
                 height: 300, // Thickness
                 color: Colors.grey.withOpacity(0.2),
-                child:  Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 18.0),
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 12.0, vertical: 18.0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
@@ -276,22 +290,26 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
                           Padding(
                             padding: const EdgeInsets.only(bottom: 15.0),
                             child: GestureDetector(
-                              onTap: (){
+                              onTap: () {
                                 Navigator.of(context)
                                     .pushNamed(LocationActivity.Tag);
                               },
                               child: CircleAvatar(
-                                backgroundColor: Colors.transparent,
-                                  child: Icon(Icons.arrow_forward, color: Colors.black,)),
+                                  backgroundColor: Colors.transparent,
+                                  child: Icon(
+                                    Icons.arrow_forward,
+                                    color: Colors.black,
+                                  )),
                             ),
                           )
                         ],
                       ),
-                      SizedBox(height: 20.0,),
+                      SizedBox(
+                        height: 20.0,
+                      ),
                       Column(
                         crossAxisAlignment: CrossAxisAlignment.center,
                         children: [
-
                           Row(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
@@ -317,7 +335,8 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
                                   Container(
                                     width: 140,
                                     child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
                                       children: [
                                         Text(
                                           "It is overall ratting of",
@@ -341,7 +360,8 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
                                 ],
                               ),
                               Padding(
-                                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                                padding: const EdgeInsets.symmetric(
+                                    horizontal: 15.0),
                                 child: Container(
                                   width: 1,
                                   height: 100, // Thickness
@@ -370,7 +390,8 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
                                   Container(
                                     width: 140,
                                     child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.center,
                                       children: [
                                         Text(
                                           "It is overall review of",
@@ -490,7 +511,6 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
               SizedBox(
                 height: 10.0,
               ),
-
               Container(
                 width: MediaQuery.of(context).size.width,
                 height: 250.0,
@@ -518,6 +538,9 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
                             fontFamily: 'Poppins',
                             fontSize: FontConstant.Mini_Tagline_text),
                       ),
+                      Container(
+                          //
+                          ),
                       SizedBox(
                         height: 20.0,
                       ),
@@ -536,18 +559,161 @@ class _LocalCandidatePageState extends State<LocalCandidatePage> {
           elevation: 0,
           backgroundColor: Colors.orangeAccent,
           splashColor: Colors.black54,
-          shape:  BeveledRectangleBorder(
-              borderRadius: BorderRadius.zero
-          ),
+          shape: BeveledRectangleBorder(borderRadius: BorderRadius.zero),
           onPressed: () {
+            showModalBottomSheet(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                backgroundColor: Colors.white,
+                context: context,
+                builder: (context) {
+                  return Padding(
+                    padding: const EdgeInsets.only(left: 22.0, right: 15.0, top: 25.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "Choose Job",
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.black,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'Poppins'),
+                            ),
+                            GestureDetector(
+                              onTap: Navigator.of(context).pop,
+                                child: CircleAvatar(
+                                  backgroundColor: Colors.transparent,
+                                  child: Image.asset(
+                              "assets/images/myicons/cancel.png",
+                              height: 20.0,
+                            ),
+                                ))
+                          ],
+                        ),
 
+                        Container(
+                          width: MediaQuery.of(context).size.width,
+                          child: Text(
+                            "Which job do you want to hire a candidate ?",
+                            style: TextStyle(
+                                color: Colors.black,
+                                fontWeight: FontWeight.w400,
+                                fontFamily: 'Poppins',
+                                fontSize: FontConstant.Tagline_text),
+                          ),
+                        ),
 
-        },
-          child: Text("SELECT JOB", style: TextStyle(
-            color: Colors.black,
-            fontFamily: 'Poppins',
-            fontWeight: FontWeight.bold,
-          ),),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                width: MediaQuery.of(context).size.width,
+                                height: 90,
+                                child: ElevatedButton(
+                                  onPressed: () {
+
+                                  },
+                                  child: Padding(
+                                    padding: const EdgeInsets.only(top: 8.0),
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      children: [
+
+                                        Row(
+                                          mainAxisAlignment: MainAxisAlignment.start,
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            GestureDetector(
+                                              onTap: () {
+
+                                              },
+                                              child:
+                                                  Padding(
+                                                    padding: const EdgeInsets.only(top:10.0),
+                                                    child: Radio(
+                                                      value: "",
+                                                      activeColor: Colors.orange,
+
+                                                      groupValue: _site,
+                                                      onChanged: ( value) {
+                                                        setState(() {
+
+                                                        });
+                                                      },
+                                                    ),
+                                                  )
+
+                                            ),
+                                         Column(
+                                           mainAxisAlignment: MainAxisAlignment.start,
+                                           crossAxisAlignment: CrossAxisAlignment.start,
+                                           children: [
+                                           Text(
+                                             "Driver",
+                                             style: TextStyle(
+                                                 fontSize: 14,
+                                                 color: Colors.black,
+                                                 fontWeight: FontWeight.bold,
+                                                 fontFamily: 'Poppins'),
+                                           ),
+                                           Container(
+                                             width: 240,
+                                             height: 40,
+                                             child: Text(
+                                               "Lorem ipsum dolor sit amet,consectetur adipiscing elit,consectetur",
+                                               maxLines: 2,
+                                               overflow: TextOverflow.ellipsis,
+                                               style: TextStyle(
+                                                   color: Colors.black,
+                                                   fontWeight: FontWeight.w400,
+                                                   fontFamily: 'Poppins',
+                                                   fontSize: 11.0),
+                                             ),
+                                           ),
+                                         ],)
+                                          ],
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  style: TextButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    elevation: 0,
+                                    primary: Colors.grey.withOpacity(0.4),
+                                    alignment: Alignment.centerLeft,
+                                    //minimumSize: Size(size.width, 45)),
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+                                child: Container(
+                                  width: MediaQuery.of(context).size.width,
+                                  height: 1, // Thickness
+                                  color: Colors.grey.withOpacity(0.2),
+                                ),
+                              ),
+                            ]),
+                      ],
+                    ),
+                  );
+                });
+          },
+          child: Text(
+            "SELECT JOB",
+            style: TextStyle(
+              color: Colors.black,
+              fontFamily: 'Poppins',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
         ),
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
